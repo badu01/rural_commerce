b4981d1c1f474f2f17c1809454714fb3a068f995
badu01 <badushapareeth777@gmail.com>
2024-11-17 23:54:55 +0530

    missing logo 192,512 added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './pages/Register';
-import Login from './pages/Login';
 import ProductListing from './pages/ProductListing';
-import InventoryManagement from './pages/InventoryManagement';
 import OrderManagement from './pages/OrderManagement';
-import Recommendations from './pages/Recommendations';
-import Analytics from './pages/Analytics';
 import Contact from './pages/Contact';
 import ItemListing from './pages/ItemsList';
 import CartPage from './pages/CartPage';
@@ -20,12 +16,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
             <Route path="/products" element={<ProductListing />} />
-            <Route path="/inventory" element={<InventoryManagement />} />
             <Route path="/orders" element={<OrderManagement />} />
-            <Route path="/recommendations" element={<Recommendations />} />
-            <Route path="/analytics" element={<Analytics />} />
             <Route path="/contact" element={<Contact />} />
             <Route path='/products/:category' element={<ItemListing/>}/>
             <Route path='/cart' element={<CartPage/>} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,19 +7,15 @@ import { useEffect } from "react";
 
 function Home() {
   useEffect(() => {
-    // Disable scrolling when Home component is mounted
     document.body.style.overflow = "hidden";
 
-    // Re-enable scrolling when the component is unmounted
     return () => {
       document.body.style.overflow = "auto";
     };
   }, []);
   return (
     <div className="flex flex-col h-screen overflow-hidden">
-      {/* Navbar */}
       <Navbar />
-      {/* Hero Section */}
       <div
         className="flex-grow h-screen flex items-center justify-center bg-cover bg-center relative"
         style={{
@@ -28,10 +24,8 @@ function Home() {
           backgroundPosition: 'center',
         }}
       >
-        {/* Overlay for Tint Effect */}
         <div className="absolute inset-0 bg-black opacity-50"></div>
 
-        {/* Hero Content */}
         <div className="relative z-10 px-4 sm:px-8 md:px-16 lg:px-24 text-white">
           <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4 text-center">
             Welcome to the Unified Urban <br className="hidden sm:block" />
@@ -48,7 +42,6 @@ function Home() {
         </div>
       </div>
 
-      {/* Footer */}
       <Footer />
     </div>
   );
