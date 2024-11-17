35513be00f4da950c9713f656c5ebd5e16507037
badu01 <badushapareeth777@gmail.com>
2024-11-18 00:25:27 +0530

    basename url added to Router

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { CartProvider } from './Constants/Values/CartContext.jsx';
 function App() {
   return (
     <CartProvider>
-      <Router>
+      <Router basename="/rural_commerce">
         <div className="App">
           <Routes>
             <Route path="/" element={<Home />} />
